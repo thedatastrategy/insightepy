c8a6a34f3d92da599da0c1541428e77a7796b006
arj-196 <arjruler93@gmail.com>
2017-06-20 17:12:09 +0200

    feat(confs) Add default conf variables

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['urllib3', 'requests', ],
+    install_requires=['urllib3', 'requests', 'configparser'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
