aa3ec9fa0c18d1f3ef2d4661e7d8993e16c85754
arj-196 <arjruler93@gmail.com>
2018-03-22 17:14:13 +0100

    chore(server): implement response pattern

--- insightepy/client.py
+++ insightepy/client.py
@@ -7,6 +7,7 @@ from urllib3 import HTTPConnectionPool
 from insightepy import conf
 from insightepy.core import Logger
 from insightepy.extractors import Extractor
+from insightepy.response import Response
 
 HOST_ADDR = conf.config.get('server', 'host')
 HOST_PORT = conf.config.get('server', 'port')
@@ -17,24 +18,43 @@ logger = Logger('InsightePy')
 
 class API(object):
     def __init__(self, client_id: str, client_secret: str, auth_token: str) -> None:
-        logger.info('Hello from InsightePy')
+        self.print_hello_message()
         self._cid = client_id
         self._csecret = client_secret
         self._auth_token = auth_token
         self.pool = HTTPConnectionPool(HOST_ADDR + ':' + HOST_PORT, timeout=2)
 
-    def make_request(self, method: str, url: str, fields: Dict[str, str]) -> Dict:
+    @staticmethod
+    def print_hello_message():
+        logger.info('--------------------------------')
+        logger.info('----- Hello from InsightePy ----')
+        logger.info('--------------------------------')
+
+    def make_request(self, method: str, url: str, fields: Dict[str, str]) -> Response:
         # adding user information to field
         fields['cid'] = self._cid
         fields['csecret'] = self._csecret
         fields['authtoken'] = self._auth_token
         logger.debug('Making request with: {}'.format(fields))
         r = self.pool.request(method, ROUTE_PREFIX + url, fields=fields)
-        try:
-            return json.loads(r.data)
-        except Exception as e:
-            logger.error('Encountered error while parsing response: {}'.format(str(e)))
-            return r.data
+        if r.status == 200:
+            logger.debug('Got raw response:  {}'.format(r.data))
+            try:
+                response = Response(json.loads(r.data))
+                logger.info('Built Response: {}'.format(response.__repr__()))
+                return response
+            except Exception as e:
+                logger.error('Encountered error while parsing response: {}'.format(str(e)))
+        else:
+            logger.error('Got a NOT OK Response Code: status={}'.format(r.status))
+
+    def say_hello(self):
+        logger.info('Running Say Hello')
+        return self.make_request(
+            'GET',
+            '/hello',
+            dict()
+        )
 
     def single_extract(self, lang: str, verbatim: str, extractors: List[Extractor] = None):
         """
